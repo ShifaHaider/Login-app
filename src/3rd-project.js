7445082f30e8aeccda18c22965fb3f06146b4f16
Shifa Haider <shifa.haider065@gmail.com>
2018-02-03 14:59:54 +0500

    login successfull

--- src/3rd-project.js
+++ src/3rd-project.js
@@ -1,31 +1,47 @@
-
-
 var accounts = JSON.parse(localStorage.getItem('accounts')) || [];
 function save() {
     var saveData1 =
-    {name: document.getElementById('name').value,
+    {
+        name: document.getElementById('name').value,
         email: document.getElementById('email').value,
-        password:  document.getElementById('password').value,
-        phoneNumber:  document.getElementById('phone').value};
+        password: document.getElementById('password').value,
+        phoneNumber: document.getElementById('phone').value
+    };
 
     var isMatch = false;
-    for (var i = 0; i< accounts.length; i++) {
+    for (var i = 0; i < accounts.length; i++) {
         if (accounts[i].email === saveData1.email) {
             isMatch = true;
         }
     }
 
-        if (isMatch) {
-            alert('email is not available')
-        }
-        else {
-            accounts.push(saveData1.email);
-            localStorage.setItem('accounts', JSON.stringify(accounts));
-        }
-
-     accounts.push(saveData1);
-   localStorage.setItem('accounts' , JSON.stringify(accounts) );
+    if (isMatch) {
+        alert('email is not available')
+    }
+    else {
+        accounts.push(saveData1);
+        localStorage.setItem('accounts', JSON.stringify(accounts));
+    }
     console.log(accounts);
 }
 
+function login() {
+    var loginData = {
+        email: document.getElementById('logEmail').value,
+        password: document.getElementById('logPassword').value
+    };
+    var logMatch = false;
+    for (var j = 0; j < accounts.length; j++) {
+        console.log(loginData);
+        if (loginData.email === accounts[j].email && loginData.password === accounts[j].password) {
+            console.log(logMatch);
+            logMatch = true;
+            break;
+        }
+    }
+    if (!logMatch) {
+        alert('account not login');
+    }
+    console.log(logMatch);
 
+}
