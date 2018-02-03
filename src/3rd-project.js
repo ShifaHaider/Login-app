b69d4fbc4f25b1e134a9e8e3e108ae8884df78c0
Shifa Haider <shifa.haider065@gmail.com>
2018-02-03 14:09:52 +0500

    adding login app

--- src/3rd-project.js
+++ src/3rd-project.js
@@ -7,6 +7,22 @@ function save() {
         email: document.getElementById('email').value,
         password:  document.getElementById('password').value,
         phoneNumber:  document.getElementById('phone').value};
+
+    var isMatch = false;
+    for (var i = 0; i< accounts.length; i++) {
+        if (accounts[i].email === saveData1.email) {
+            isMatch = true;
+        }
+    }
+
+        if (isMatch) {
+            alert('email is not available')
+        }
+        else {
+            accounts.push(saveData1.email);
+            localStorage.setItem('accounts', JSON.stringify(accounts));
+        }
+
      accounts.push(saveData1);
    localStorage.setItem('accounts' , JSON.stringify(accounts) );
     console.log(accounts);
