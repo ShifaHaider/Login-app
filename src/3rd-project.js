ae69c05496200949ba884a60c118d9db8b5193cb
Shifa Haider <shifa.haider065@gmail.com>
2018-02-06 13:23:38 +0500

    Complete Sign up & Login app

--- src/3rd-project.js
+++ src/3rd-project.js
@@ -28,11 +28,13 @@ function login() {
         password: document.getElementById('logPassword').value
     };
     var logMatch = false;
+    var div = document.getElementById('madeDiv');
     for (var j = 0; j < accounts.length; j++) {
-        console.log(loginData);
+        var inp = document.createElement('input');
         if (loginData.email === accounts[j].email && loginData.password === accounts[j].password) {
             localStorage.setItem('userData' , JSON.stringify(accounts[j]));
-            alert('Welcome ' + accounts[j].name);
+            //alert('Welcome ' + accounts[j].name);
+            resultForLogin(accounts[j]);
             console.log(logMatch);
             logMatch = true;
             break;
@@ -43,3 +45,23 @@ function login() {
     }
     console.log(logMatch);
 }
+function resultForLogin(user) {
+    var div = document.getElementById('madeDiv');
+    div.style.border = '1px solid #000' ;
+    div.style. width = '220px';
+    var h3 = document.createElement('h3');
+    h3.innerHTML = 'User Data';
+    var p1 = document.createElement('p');
+    var p2 = document.createElement('p');
+    var p3 = document.createElement('p');
+    p1.innerHTML = 'Name : ' + user.name;
+    p2.innerHTML = 'Email : ' + user.email;
+    p3.innerHTML = 'Phone Number : ' + user.phoneNumber;
+    div.appendChild(h3);
+    div.appendChild(p1);
+    div.appendChild(p2);
+    div.appendChild(p3);
+
+console.log(div);
+}
+
